40957a851232ca942ebdfdfdd4bc8769ad342c5f
blackstone_99 <imbistone@gmail.com>
2023-07-30 11:58:39 +0800

    fix viz bugs: remove nan number

--- train/modules/visualize.py
+++ train/modules/visualize.py
@@ -9,7 +9,7 @@ class PlotAfterTrain(Callback):
         pde_loss_cols = [col for col in cols if col.startswith('pde_loss')]
         bc_loss_cols = [col for col in cols if col.startswith('bc_loss')]
         l2_error_cols = [col for col in cols if col.startswith('l2_err')]
-        plot_history(pinn.config.log_filename, pde_loss_cols, pathlib.Path(pinn.config.log_dir) / 'pde_loss.png')
-        plot_history(pinn.config.log_filename, bc_loss_cols, pathlib.Path(pinn.config.log_dir) / 'bc_loss.png')
-        plot_history(pinn.config.log_filename, l2_error_cols, pathlib.Path(pinn.config.log_dir) / 'l2_error.png')
+        plot_history(pinn.logger.df, pde_loss_cols, pathlib.Path(pinn.config.log_dir) / 'pde_loss.png')
+        plot_history(pinn.logger.df, bc_loss_cols, pathlib.Path(pinn.config.log_dir) / 'bc_loss.png')
+        plot_history(pinn.logger.df, l2_error_cols, pathlib.Path(pinn.config.log_dir) / 'l2_error.png')
             
--- visualize/visual_history.py
+++ visualize/visual_history.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from utils.csv import get_cols
 
 
-def plot_history(log_filename: str, columns: list, save_path: str):
+def plot_history(df: pd.DataFrame, columns: list, save_path: str):
     """plot value history from log file, columns is a list of column names
         compare different columns of the same log file
     """
-    df = pd.read_csv(log_filename)
     fig, ax = plt.subplots()
     fig.set_size_inches(12, 8)
     ax.set_xlabel('epoch')
     ax.set_ylabel('value')
     for column in columns:
-        ax.plot(df['epoch'], df[column], label=column)
+        x, y = get_cols(df, column)
+        ax.plot(x, y, label=column)
     ax.legend()
     fig.savefig(save_path)
     
@@ -36,23 +37,4 @@ def compare_history(log_filenames: list, column: str, save_path: str):
     
 
 if __name__ == '__main__':
-    # files = ['logs/adaptive loss.csv', 'logs/without adaptive loss.csv', 'logs/ns_RAR_D.csv']
-    # compare_history(files, 'l2_err_0', 'l2_err_0.png')
-    # compare_history(files, 'l2_err_1', 'l2_err_1.png')
-    
-    # plot_history('logs/adaptive loss.csv', ['w1', 'w2'], 'adaptive loss weights history.png')
-    # plot_history('logs/adaptive loss.csv', ['log_sigma1', 'log_sigma2'], 'adaptive loss log_sigma history.png')
-    # plot_history('logs/adaptive loss.csv', ['pde_loss_0', 'pde_loss_1'], 'adaptive loss pde_loss history.png')
-    # plot_history('logs/adaptive loss.csv', ['boundary_loss_0', 'boundary_loss_1'], 'adaptive loss boundary_loss history.png')
-    # plot_history('logs/adaptive loss.csv', ['loss'], 'adaptive loss history.png')
-    
-    
-    files = ['logs/burgers_ada_loss.csv', 'logs/burgers_without_ada_loss.csv']
-    # compare_history(files, 'l2_err_0', 'burgers_l2_err_0.png')
-    
-    plot_history('logs/burgers_ada_loss.csv', ['w1', 'w2'], 'burgers adaptive loss weights history.png')
-    # plot_history('logs/burgers_ada_loss.csv', ['log_sigma1', 'log_sigma2'], 'burgers adaptive loss log_sigma history.png')
-    # plot_history('logs/burgers_ada_loss.csv', ['pde_loss_0'], 'burgers adaptive loss pde_loss history.png')
-    # plot_history('logs/burgers_ada_loss.csv', ['bc_loss_0'], 'burgers adaptive loss boundary_loss history.png')
-    # plot_history('logs/burgers_ada_loss.csv', ['loss'], 'burgers adaptive loss history.png')
-        
+    ...
